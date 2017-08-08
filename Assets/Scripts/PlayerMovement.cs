9936f55630a086d992e3138f712aa38e5897ecfa
Jacktivity <jh140695@gmail.com>
2017-08-08 19:47:05 +0100

    Character
    Character part switching is in place, platform is on, mouse following
    and shooting is in place, basic lighting is in, character movement is
    in.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float xSpeed;
+    public float ySpeed;
+    float rotate;
+    public float startXSpeed;
+    public float startYSpeed;
+    public float startRotate;
+    public bool platformActive;
+    public GameObject legs;
+    public bool facingRight;
+    // Use this for initialization
+    void Start ()
+    {
+        startXSpeed = xSpeed;
+        startYSpeed = ySpeed;
+        rotate = 2;
+        
+	}
+	// Update is called once per frame
+	void Update ()
+    {
+        if(legs == null)
+        {
+            findLegs();
+        }
+		if(Input.GetKey(KeyCode.D))
+        {
+            xSpeed = startXSpeed;
+            rotate = 2;
+            facingRight = true;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            xSpeed = -startXSpeed;
+            rotate = -2;
+            facingRight = false;
+        }
+        else
+        {
+            xSpeed = 0;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ySpeed = startYSpeed;
+        }
+        gameObject.transform.position = new Vector2(gameObject.transform.position.x + xSpeed, gameObject.transform.position.y + ySpeed);
+        gameObject.transform.localScale = new Vector3(rotate, 2, 2);
+        ySpeed = 0;
+
+        platformActive = legs.GetComponent<ChangePart>().activatePlatform;
+    }
+    public void findLegs()
+    {
+        legs = GameObject.FindGameObjectWithTag("Leg");
+    }
+}
---
+++ Assets/Scripts/TrackMouse.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackMouse : MonoBehaviour {
+    GameObject player;
+    GameObject spawnPoint;
+    public GameObject bullet;
+    public Vector3 lookingPos;
+    float angle;
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+        lookPos = lookPos - transform.position;
+        if (player.GetComponent <PlayerMovement>().facingRight == true)
+        {
+            angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+        }
+        else
+        {
+            angle = Mathf.Atan2(-lookPos.y, -lookPos.x) * Mathf.Rad2Deg;
+        }
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed left click.");
+            GameObject newBullet = Instantiate(bullet, new Vector3(spawnPoint.transform.position.x, spawnPoint.transform.position.y, spawnPoint.transform.position.z), spawnPoint.transform.rotation) as GameObject;
+        }
+    }
+}
